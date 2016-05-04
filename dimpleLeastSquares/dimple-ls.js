7ce47cb5689846ae4e66a070d560618e40341199
Jacob Schulzetenberg <Jacob.Schulzetenberg@gmail.com>
2016-05-04 11:30:01 -0600

    Fix dimple trend line

--- dimpleLeastSquares/dimple-ls.js
+++ dimpleLeastSquares/dimple-ls.js
@@ -5,7 +5,7 @@ d3.tsv("http://dimplejs.org/data/example_data.tsv", function (data) {
 
   for(var i=0; i < data.length; i++){
       if(data[i]["Channel"] === "Supermarkets"){
-        var epochDate = moment(data[i]["Month"], "MMM-DD").valueOf();
+        var epochDate = moment(data[i]["Month"], "MMM-YY").valueOf();
         x.push(epochDate);
 
         unitsInt = parseInt(data[i]["Unit Sales"]);
@@ -17,7 +17,7 @@ d3.tsv("http://dimplejs.org/data/example_data.tsv", function (data) {
 
   for(var i=0; i < result[0].length; i++) {
     data.push({
-      "Month":  moment(result[0][i]).format("MMM-DD"),
+      "Month":  moment(result[0][i]).format("MMM-YY"),
       "Unit Sales": result[1][i],
       "Channel": "Trend Line Supermarkets"
     });
@@ -26,7 +26,7 @@ d3.tsv("http://dimplejs.org/data/example_data.tsv", function (data) {
   var myChart = new dimple.chart(svg, data);
   myChart.setBounds(60, 30, 505, 305)
   var x = myChart.addCategoryAxis("x", "Month");
-  // x.addOrderRule("Date");
+  x.addOrderRule("Date");
   myChart.addMeasureAxis("y", "Unit Sales");
   myChart.addSeries("Channel", dimple.plot.line);
   myChart.addLegend(180, 10, 360, 20, "right");
